c2189432c5d5988b71a98b4d35757eed8cc2d793
Gilad Weiss <weiss.gilad@gmail.com>
2021-11-15 00:00:38 +0200

    so apparently submodules aren't supposed to be in gitignore. also, typo

--- src/Utils/constexpr_utils.h
+++ src/Utils/constexpr_utils.h
@@ -8,7 +8,7 @@ uint32_t constexpr constexpr_hash(const char *pInput, unsigned int uPrev = 5381)
 template <typename...Args>
 auto constexpr do_nothing_return_last(Args&&...args) {
     // comma operator discards the result of the left
-    //  must be in () because of it's low precedence
+    //  must be in () because of its low precedence
     //  calling a function with it gets rid of the 'no effect' warning so std::forward it is
     return (std::forward<Args>(args), ...);
 }
